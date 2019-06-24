0830e90509bca4a083c36f9635d38d58b266ed73
Jim Hough <teachlife@outlook.com>
2019-06-24 00:34:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,4 +9,10 @@ class App < Sinatra::Base
   get '/square/:number' do
     ((params[:number].to_i)**2).to_s
   end
+
+  get '/say/:number/:phrase' do
+    params[:number].times do 
+      params[:phrase]
+    end
+  end
 end
